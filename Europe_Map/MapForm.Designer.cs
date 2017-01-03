810d66ef70f738ff59b68e3b33da0af4176948bb
ZhluktenkoDmytro <d1mnewz@gmail.com>
2017-01-03 13:53:10 +0200

    Add project files.

---
+++ Europe_Map/MapForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Europe_Map
+{
+    partial class MapForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapForm));
+            this.GroupBox = new System.Windows.Forms.GroupBox();
+            this.ButtonLabelsBox = new System.Windows.Forms.GroupBox();
+            this.ShowLabelsButton = new System.Windows.Forms.Button();
+            this.MapPBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.GroupBox.SuspendLayout();
+            this.ButtonLabelsBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MapPBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GroupBox
+            // 
+            this.GroupBox.Controls.Add(this.ButtonLabelsBox);
+            this.GroupBox.Controls.Add(this.MapPBox);
+            this.GroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GroupBox.Location = new System.Drawing.Point(0, 0);
+            this.GroupBox.Name = "GroupBox";
+            this.GroupBox.Size = new System.Drawing.Size(970, 533);
+            this.GroupBox.TabIndex = 0;
+            this.GroupBox.TabStop = false;
+            // 
+            // ButtonLabelsBox
+            // 
+            this.ButtonLabelsBox.Controls.Add(this.ShowLabelsButton);
+            this.ButtonLabelsBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ButtonLabelsBox.Location = new System.Drawing.Point(3, 16);
+            this.ButtonLabelsBox.Name = "ButtonLabelsBox";
+            this.ButtonLabelsBox.Size = new System.Drawing.Size(216, 514);
+            this.ButtonLabelsBox.TabIndex = 1;
+            this.ButtonLabelsBox.TabStop = false;
+            this.ButtonLabelsBox.Enter += new System.EventHandler(this.ButtonLabelsBox_Enter);
+            // 
+            // ShowLabelsButton
+            // 
+            this.ShowLabelsButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ShowLabelsButton.Location = new System.Drawing.Point(3, 463);
+            this.ShowLabelsButton.Name = "ShowLabelsButton";
+            this.ShowLabelsButton.Size = new System.Drawing.Size(210, 48);
+            this.ShowLabelsButton.TabIndex = 0;
+            this.ShowLabelsButton.Text = "Показати хід зародження фашизму в Європі";
+            this.ShowLabelsButton.UseVisualStyleBackColor = true;
+            this.ShowLabelsButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MapPBox
+            // 
+            this.MapPBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MapPBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.MapPBox.Image = ((System.Drawing.Image)(resources.GetObject("MapPBox.Image")));
+            this.MapPBox.Location = new System.Drawing.Point(225, 16);
+            this.MapPBox.Name = "MapPBox";
+            this.MapPBox.Size = new System.Drawing.Size(742, 514);
+            this.MapPBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MapPBox.TabIndex = 0;
+            this.MapPBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MapForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 533);
+            this.Controls.Add(this.GroupBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MapForm";
+            this.Text = "Зародження фашизму в Європі";
+            this.GroupBox.ResumeLayout(false);
+            this.ButtonLabelsBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MapPBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GroupBox;
+        private System.Windows.Forms.PictureBox MapPBox;
+        private System.Windows.Forms.GroupBox ButtonLabelsBox;
+        private System.Windows.Forms.Button ShowLabelsButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Europe_Map/MapForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Europe_Map
+{
+    //    Назва проекту: Зародження фашизму в Європі.Завдання як таких два (Visual
+    //Studio Windows Form C#).
+    //1. Необхідно взяти карту Європи, можливо 30-40х років, якщо не получиться
+    //то на сучасній, на ній необхідно щоб були обов'язково наступні країни:
+    //Італія
+    //Німеччина
+    //Іспанія
+    //Болгарія
+    //Угорщина
+    //Румунія
+    //Югославія
+    //Португалія
+    // Коли наводиш на любу з перелічених вище країн вона підсвічується і можна
+    //перейти на іншу форму.
+    //2. Вихідна та сама карта, по кнопці за таймером темніють або міняють колір
+    //на темний країни, висвічується назва країни та дата в наступній
+    //послідовності:
+    //Італія - 28 жовтня 1921 року
+    //Болгарія - 9 вересня 1923 року
+    //Португалія - 1926 рік
+    //Угорщина - вересень 1932 року
+    //Німеччина - 28 лютого 1933 року
+    //Румунія - 1934 рік
+    //Югославія - лютий 1939 року
+    //Іспанія - кінець березня 1939 року
+    //Коли країна погасла вона і залишається темною.
+    public partial class MapForm : Form
+    {
+        // form for task 2
+        // realization via a bunch of pictures with faded countries.
+
+        // or to cut every country and fade them one by one
+        List<Control> Labels = new List<Control>();
+        int counter = 0;
+        public MapForm()
+        {
+            InitializeComponent();
+            //listView1.Items.Add("hello");
+            //listView1.Items[0].Selected = true;
+            //listView1.RedrawItems();
+            InitLabels();
+            ButtonLabelsBox.Controls.AddRange(Labels.ToArray());
+            this.timer1.Interval = 2000;
+            //this.timer1.Start();
+        }
+
+
+        public void InitLabels()
+        {
+            int topMargin = 20;
+            Font f = new Font("Arial", 9);
+            for (int i = 0; i < 8; i++)
+            {
+                Labels.Add(new Label() { Font = f, Visible = false, Top = topMargin, AutoSize = true, Left = 5 });
+                topMargin += 30;
+            }
+            Labels[0].Text = "Італія - 28 жовтня 1921 року";
+            Labels[1].Text = "Болгарія - 9 вересня 1923 року";
+            Labels[2].Text = "Португалія - 1926 рік";
+            Labels[3].Text = "Угорщина - вересень 1932 року";
+            Labels[4].Text = "Німеччина - 28 лютого 1933 року";
+            Labels[5].Text = "Румунія - 1934 рік";
+            Labels[6].Text = "Югославія - лютий 1939 року";
+            Labels[7].Text = "Іспанія - кінець березня 1939 року";
+
+
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            this.ShowLabelsButton.Enabled = false;
+            this.timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Labels[counter].ForeColor = Color.Black;
+
+            this.Labels[counter].Visible = true;
+            
+            switch (counter)
+            {
+                case 0:
+                    this.MapPBox.Image = Properties.Resources.first_italy;
+                    break;
+                case 1:
+                    this.MapPBox.Image = Properties.Resources.second_bulgary;
+                    break;
+                case 2:
+                    this.MapPBox.Image = Properties.Resources.third_portugal;
+                    break;
+                case 3:
+                    this.MapPBox.Image = Properties.Resources.fourth_hungary;
+                    break;
+                case 4:
+                    this.MapPBox.Image = Properties.Resources.fifth_germany;
+                    break;
+                case 5:
+                    this.MapPBox.Image = Properties.Resources.sixth_romania;
+                    break;
+                case 6:
+                    this.MapPBox.Image = Properties.Resources.seventh_yugoslavia;
+                    break;
+                case 7:
+                    this.MapPBox.Image = Properties.Resources.eighth_spain;
+                    break;
+            }
+            
+            this.MapPBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.MapPBox.Refresh();
+
+            counter++;
+            if (counter == 8)
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void ButtonLabelsBox_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
